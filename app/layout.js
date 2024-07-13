2976be004ed0c7b443a3ab4bf01898de62011bf8
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-13 21:09:26 +0200

    feat: Add Spinner, SpinnerMini, and Logo components
    This commit adds the Spinner, SpinnerMini, and Logo components to the codebase. The Spinner component displays a loading spinner, the SpinnerMini component displays a smaller loading spinner, and the Logo component renders the logo and name of the application. These components enhance the visual appeal and branding of the application.
    
    Co-authored-by: [Jonas S.]

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,17 @@
+import Logo from "./components/Logo";
 import Navigation from "./components/Navigation";
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <Navigation />
-      <body>{children}</body>
+      <body>
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+        <main>{children}</main>
+        <footer>&copy; 2021 Wild Oasis &bull; All rights reserved</footer>
+      </body>
     </html>
   );
 }

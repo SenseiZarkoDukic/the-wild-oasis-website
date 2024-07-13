bb02b52e74682800e004006d141cf8fcd0a2a8a5
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-13 23:10:40 +0200

    feat: Update Cabins page to fetch and display user data
    The Cabins page component has been updated to fetch user data from the API endpoint "https://jsonplaceholder.typicode.com/users" and display it in the console. This enhancement improves the functionality of the page by retrieving and logging the data.

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,9 @@
-
-export default function Page() {
-
-const res = await fetch('https://jsonplaceholder.typicode.com/users')
-
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
   return (
     <div>
-    
       <h1>Cabins page</h1>
     </div>
   );

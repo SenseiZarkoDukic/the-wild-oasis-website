493c31b70f1647b13ba2778ee48a57016bbec738
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-13 02:30:16 +0200

    refactor app layout

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,9 @@
-import Navigation from "../components/Navigation";
+
 
 export default function Page() {
   return (
     <div>
-      <Navigation />
+     
       <h1>About the Wild Oasis</h1>
     </div>
   );

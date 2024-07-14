d99058614fa71ab824180a617fdb7668c8830b96
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-14 17:59:56 +0200

    refactor: Update body class in layout.js to use primary background color

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ import "@/app/_styles/globals.css";
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body className="bg-blue-900 text-gray-50 min-h-screen">
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />

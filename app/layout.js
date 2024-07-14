173c58d5f13b36152c86d357c53350913c9cabb6
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-14 18:28:14 +0200

    refactor: Update description in layout.js
    The description in layout.js has been updated to provide a more detailed overview of the luxurious cabin hotel located in the Italian Dolomites. This refactor aligns with recent user commits and repository commits that involve updating page metadata titles and import paths for Logo and Navigation components in layout.js.

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,8 @@ export const metadata = {
     template: "%s / The Wild Oasis",
     default: "Welcome / The Wild Oasis",
   },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and  dark forests.",
 };
 
 export default function RootLayout({ children }) {

65e308488a8cfe607624d419cd50dc04d4cfd413
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-28 23:59:22 +0200

    refactor: Update Error component to accept error prop
    The Error component in the app/error.js file has been updated to accept an error prop. This refactor aligns with recent user commits and repository commits that involve updating components and improving error handling.

--- app/error.js
+++ app/error.js
@@ -1,4 +1,6 @@
-export default function Error() {
+"use client";
+
+export default function Error({ error }) {
   return (
     <main className="flex justify-center items-center flex-col gap-6">
       <h1 className="text-3xl font-semibold">Something went wrong!</h1>

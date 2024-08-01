fb002bce1bdb8ebf78f1a3dd663557910b2a804b
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-08-01 19:05:37 +0200

    refactor: Update CabinList component to use displayedCabins instead of cabins

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -28,7 +28,7 @@ async function CabinList({ filter }) {
 
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>

4a50444841334f08729b4a08cc642a7d04ac6deb
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-08-06 19:20:11 +0200

    refactor: Update Reservation component to retrieve settings and booked dates for specified cabin

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -2,10 +2,10 @@ import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
 
-async function Reservation({ params }) {
+async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
-    getBookedDatesByCabinId(params.cabinId),
+    getBookedDatesByCabinId(cabin.id),
   ]);
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">

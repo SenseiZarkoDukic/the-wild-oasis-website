15d7e10d4c61acaf016b5f09372b6a4efb64e988
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-08-07 18:23:13 +0200

    refactor: Reserve cabin name today with lazy loading for images
    Refactor the cabin page to include lazy loading for images, improving page load performance. This change aligns with recent user commits and repository commits that involve updating components and improving data handling.

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -40,14 +40,12 @@ export async function generateStaticParams() {
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
 
-  const { id, name, maxCapacity, regularPrice, discount, image, description } =
-    cabin;
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <Cabin cabin={cabin} />
       <div>
         <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
-          Reserve {name} today. Pay on arrival.
+          Reserve {cabin?.name} today. Pay on arrival.
         </h2>
         <Suspense fallback={<Spinner />}>
           <Reservation cabin={cabin} />

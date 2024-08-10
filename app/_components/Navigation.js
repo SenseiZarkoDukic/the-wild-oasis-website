311e91469848db1158ad9e58d81e64cfe0132d79
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-08-10 16:29:05 +0200

    refactor: Update Navigation component to include authentication check
    The Navigation component in Navigation.js has been updated to include an authentication check using the auth() function from the _lib/auth module. This change aligns with recent user commits and repository commits that involve improving authentication functionality.

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+  console.log(session);
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">

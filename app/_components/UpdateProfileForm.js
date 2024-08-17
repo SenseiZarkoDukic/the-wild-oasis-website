09dada9286a2a3ea2e639d0f6c111b162b037f31
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-08-17 04:22:06 +0200

    refactor: Update UpdateProfileForm.js to include guest data in form fields
    The UpdateProfileForm component in UpdateProfileForm.js has been updated to include the guest data in the form fields. This change ensures that the full name, email, country flag, nationality, and national ID are correctly displayed in the input fields. It aligns with recent user commits and repository commits that involve enhancing the profile update process and improving the UpdateProfileForm component.

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,18 +1,18 @@
 "use client";
 import { useState } from "react";
 
-function UpdateProfileForm({ children }) {
+function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState(0);
 
-  const countryFlag = "pt.jpg";
+  const { fullName, email, countryFlag, nationality, nationalID } = guest;
 
-  const nationality = "portugal";
   return (
     <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>

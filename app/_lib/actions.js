deb1043ffdb93d023730fb71a1ef1951bce1fabd
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-08-18 12:03:42 +0200

    refactor: Update updateGuest function to include guest ID in update query
    The updateGuest function in actions.js has been updated to include the guest ID in the update query. This change ensures that the correct guest record is updated based on the guest ID.

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 
@@ -31,6 +32,8 @@ export async function updateGuest(formData) {
     console.error(error);
     throw new Error("Guest could not be updated");
   }
+
+  revalidatePath("/account/profile");
 }
 
 export async function signInAction() {
